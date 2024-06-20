07dd5998f2dbbcd24f98b78a15b9f1b26cc9fb1e
igorgabs05 <>
2024-06-20 09:15:52 -0300

    20-06

--- historia.php
+++ historia.php
@@ -4,17 +4,28 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Treine em casa</title>
+	<title>Origem</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-	<link rel="stylesheet" type="text/css" href="css/fundamentos.css">
-	<link rel="stylesheet" href="css/navbar.css">
+	<link rel="stylesheet" href="../css/historia.css">
 </head>
 <body>
+	<div class="card-body">
 	<h1>Origem do Voleibol</h1>
 	<p>William George Morgan, professor e diretor de Educação Física na Associação Cristã de Moços (ACM), idealizou um jogo em que as probabilidades dos participantes se machucarem, em detrimento de contato físico, fossem baixas. Outro fator importante - e pensado nos jogadores mais velhos - era que o esporte não fosse fisicamente tão exigente.</p>
 	<p>Na época, o basquetebol estava na moda. Havia sido criado 4 anos antes, em 1891, também por um professor de educação física da Associação Cristã dos Moços.</p>
-	<p>E assim surgiu o vôlei - chamado de "mintonette" - que, graças ao fato de cada uma das equipes ficarem separadas por uma rede, trazia menos chances de lesões.</p>
+	<p>E assim surgiu o vôlei - chamado de "mintonette" - que, graças ao fato de cada uma das equipes ficarem separadas por uma rede, trazia menos chances de lesões. Além disso, era menos exigente em termos físicos do que o basquetebol - uma vez que os jogadores não precisavam correr ao longo do jogo por toda a quadra. O vôlei, no entanto, era muito completo, pois compreendia uma série de exercícios benéficos para a saúde.</p>
+	<p>Em seguida, foi a vez de tratar das regras do jogo. Para tanto, Morgan contou com o auxílio de dois amigos, Dr. Frank Wood e John Lynch.</p>
+	<p>No ano seguinte da sua criação, Morgan foi convidado a fazer uma demonstração do esporte em um encontro promovido pela Associação Cristã dos Moços. Tratava-se, na verdade, do Congresso de Professores de Educação Física, que reuniu os conselheiros de educação física da ACM.</p>
+	<p>Nessa ocasião, a alteração do nome “mintonette” para “volley ball” foi sugerida pelo professor Alfred T. Halstead, porque os movimentos do novo jogo sugeriam isso, um voleio, ou seja, uma jogada feita no ar. Por fim, os conselheiros receberem uma cópia das regras do jogo, e uma equipe ficou responsável por estudá-las e sugerir melhorias.</p>
+	<h2>A chegada do vôlei em vários países do mundo</h2>
+	<p>Dos Estados Unidos da América, o vôlei seguiu para o Canadá, chegando à América do Sul em 1910. O primeiro país sul-americano a conhecê-lo foi o Peru, e em 1915 foi a vez do Brasil. Em 1951, foi no Brasil que o primeiro campeonato sul-americano de vôlei foi disputado.</p>
+	<p>Em 20 de abril de 1947 foi fundada a Federação Internacional de Voleibol (FIVB), que teve como primeiro presidente Paul Libaud. Quando fundada, tinha 14 membros: Brasil, Bélgica, Egito, Estados Unidos, França, Holanda, Hungria, Itália, Iugoslávia, Polônia, Portugal, Romênia, Checoslováquia e Uruguai.
+Atualmente, a FIVB conta com 163 países filiados.</p>
+	<p>A FIVB é o órgão responsável por todas as questões que envolvem o voleibol, tais como a organização de torneios e congressos, além de oferecer cursos para árbitros e treinadores.</p>
+	<p>Dois anos depois da fundação da Federação, em 1949, realizou-se o primeiro campeonato de vôlei masculino na Checoslováquia, o qual foi conquistado pelos russos. Em 1952 foi a vez das mulheres disputaram pela primeira vez um campeonato de voleibol, sendo o título conquistado pelas russas.</p>
+	<p>Quase 70 anos após a sua criação, a partir de 1964, o vôlei passou a integrar os jogos olímpicos. A Rússia foi o primeiro campeão olímpico do vôlei masculino, quando a Checoslováquia ficou com a prata e o Japão, com o bronze.</p>
+	</div>
 	<?php 
     	include 'footer.php';
   	?>
