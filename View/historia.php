3b7e2202c3e6e196199143a21930ab4071398659
Sara Beraldo <saraberaldo28@gmail.com>
2024-06-24 12:33:44 -0300

    12:33 - 24/04/24

--- View/historia.php
+++ View/historia.php
@@ -4,40 +4,113 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Treine em casa</title>
+	<title>Origem</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-	<link rel="stylesheet" type="text/css" href="css/fundamentos.css">
-	<link rel="stylesheet" href="css/navbar.css">
+	<link rel="stylesheet" href="../View/css/historia.css">
 </head>
 <body>
 	<?php 
 		include 'navbar.php';
 	?>
-	<main>
-		<ul class="nav nav-tabs" style="position: relative; margin-top: 20px;">
-			<li class="nav-item">
-				<a class="nav-link" aria-current="page" href="fundamentos.php"  style="color: #363636;">Fundamentos</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link active" aria-current="page" href="historia.php"  style="color: #363636;">História</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" aria-current="page" href="regras.php"  style="color: #363636;">Regras</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" aria-current="true" href="sistema-rodizio.php"  style="color: #363636;">Sistema de Rodízio</a>
-			</li>
-		</ul>
-		<h1>Origem do Voleibol</h1>
-		<p>William George Morgan, professor e diretor de Educação Física na Associação Cristã de Moços (ACM), idealizou um jogo em que as probabilidades dos participantes se machucarem, em detrimento de contato físico, fossem baixas. Outro fator importante - e pensado nos jogadores mais velhos - era que o esporte não fosse fisicamente tão exigente.</p>
-		<p>Na época, o basquetebol estava na moda. Havia sido criado 4 anos antes, em 1891, também por um professor de educação física da Associação Cristã dos Moços.</p>
-		<p>E assim surgiu o vôlei - chamado de "mintonette" - que, graças ao fato de cada uma das equipes ficarem separadas por uma rede, trazia menos chances de lesões.</p>
-	</main>
-	<footer>
-		<?php 
-    		include 'footer.php';
-  		?>
-	</footer>
+	<ul class="nav nav-tabs d-flex justify-content-center" style="position: relative; margin-top: 20px;">
+		<li class="nav-item">
+			<a class="nav-link" aria-current="page" href="fundamentos.php"  style="color: #363636;">Fundamentos</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link active" aria-current="page" href="historia.php"  style="color: #363636;">História</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" aria-current="page" href="regras.php"  style="color: #363636;">Regras</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" aria-current="true" href="sistema-rodizio.php"  style="color: #363636;">Sistema de Rodízio</a>
+		</li>
+	</ul>
+
+	<div class="card-body">
+	<h1>Origem do Voleibol</h1>
+
+	<p>William George Morgan, professor e diretor de Educação Física na Associação Cristã de Moços (ACM), idealizou um jogo em que as probabilidades dos participantes se machucarem, em detrimento de contato físico, fossem baixas. Outro fator importante - e pensado nos jogadores mais velhos - era que o esporte não fosse fisicamente tão exigente.</p>
+	<p>Na época, o basquetebol estava na moda. Havia sido criado 4 anos antes, em 1891, também por um professor de educação física da Associação Cristã dos Moços.</p>
+	<p>E assim surgiu o vôlei - chamado de "mintonette" - que, graças ao fato de cada uma das equipes ficarem separadas por uma rede, trazia menos chances de lesões. Além disso, era menos exigente em termos físicos do que o basquetebol - uma vez que os jogadores não precisavam correr ao longo do jogo por toda a quadra. O vôlei, no entanto, era muito completo, pois compreendia uma série de exercícios benéficos para a saúde.</p>
+	<p>Em seguida, foi a vez de tratar das regras do jogo. Para tanto, Morgan contou com o auxílio de dois amigos, Dr. Frank Wood e John Lynch.</p>
+	<p>No ano seguinte da sua criação, Morgan foi convidado a fazer uma demonstração do esporte em um encontro promovido pela Associação Cristã dos Moços. Tratava-se, na verdade, do Congresso de Professores de Educação Física, que reuniu os conselheiros de educação física da ACM.</p>
+	<p>Nessa ocasião, a alteração do nome “mintonette” para “volley ball” foi sugerida pelo professor Alfred T. Halstead, porque os movimentos do novo jogo sugeriam isso, um voleio, ou seja, uma jogada feita no ar. Por fim, os conselheiros receberem uma cópia das regras do jogo, e uma equipe ficou responsável por estudá-las e sugerir melhorias.</p>
+	
+	<h2>A chegada do vôlei em vários países do mundo</h2>
+
+	<p>Dos Estados Unidos da América, o vôlei seguiu para o Canadá, chegando à América do Sul em 1910. O primeiro país sul-americano a conhecê-lo foi o Peru, e em 1915 foi a vez do Brasil. Em 1951, foi no Brasil que o primeiro campeonato sul-americano de vôlei foi disputado.</p>
+	<p>Em 20 de abril de 1947 foi fundada a Federação Internacional de Voleibol (FIVB), que teve como primeiro presidente Paul Libaud. Quando fundada, tinha 14 membros: Brasil, Bélgica, Egito, Estados Unidos, França, Holanda, Hungria, Itália, Iugoslávia, Polônia, Portugal, Romênia, Checoslováquia e Uruguai.
+Atualmente, a FIVB conta com 163 países filiados.</p>
+	<p>A FIVB é o órgão responsável por todas as questões que envolvem o voleibol, tais como a organização de torneios e congressos, além de oferecer cursos para árbitros e treinadores.</p>
+	<p>Dois anos depois da fundação da Federação, em 1949, realizou-se o primeiro campeonato de vôlei masculino na Checoslováquia, o qual foi conquistado pelos russos. Em 1952 foi a vez das mulheres disputaram pela primeira vez um campeonato de voleibol, sendo o título conquistado pelas russas.</p>
+	<p>Quase 70 anos após a sua criação, a partir de 1964, o vôlei passou a integrar os jogos olímpicos. A Rússia foi o primeiro campeão olímpico do vôlei masculino, quando a Checoslováquia ficou com a prata e o Japão, com o bronze.</p>
+	</div>
+
+	<h2>Os 20 melhores jogadores brasileiros de vôlei de todos os tempos</h2>
+
+	<p> 1. Giba<br>
+	Títulos: Quatro vezes medalhista olímpico (duas vezes ouro), tricampeão mundial, oito vezes campeão da Liga Mundial.<br>
+	Equipes Notáveis: Minas Tênis Clube, Italiano Sisley Treviso, Dentil Praia Clube.<br>
+	Curiosidades: Giba é amplamente reconhecido como um dos melhores jogadores de vôlei de todos os tempos, conhecido por sua habilidade atlética e carisma.
+	</p>
+
+	<p> 2. Bernardinho<br>
+	Títulos: Medalhista de ouro olímpico como jogador (1984) e como técnico (2004, 2016), além de diversos títulos na Liga Mundial e no Campeonato Mundial.<br>
+	Equipes Notáveis: Fluminense, Seleção Brasileira Masculina de Vôlei.<br>
+	Curiosidades: Além de ser um jogador excepcional, Bernardinho se destacou ainda mais como técnico, guiando a Seleção Brasileira a várias conquistas históricas.
+	</p>
+
+	<p> 3. Serginho<br>
+	Títulos: Quatro medalhas olímpicas (duas de ouro), além de múltiplos títulos mundiais e da Liga Mundial.<br>
+	Equipes Notáveis: SESI-SP, Trentino Volley.<br>
+	Curiosidades: Conhecido por sua incrível habilidade defensiva e liderança em quadra, Serginho é uma lenda do vôlei brasileiro.
+	</p>
+	
+	<p> 4. Bruno Rezende (Bruninho)<br>
+	Títulos: Medalhista de ouro olímpico (2016), campeão da Liga Mundial e do Campeonato Mundial.<br>
+	Equipes Notáveis: Modena Volley, SESI-SP.<br>
+	Curiosidades: Como filho do ex-jogador Bernardinho, Bruninho seguiu os passos do pai e se tornou um dos melhores levantadores do mundo.
+	</p>
+
+	<p> 5. Nalbert Bitencourt<br>
+	Títulos: Medalhista de ouro olímpico (2004), campeão da Liga Mundial e do Campeonato Mundial.<br>
+	Equipes Notáveis: Minas Tênis Clube, Modena Volley.<br>
+	Curiosidades: Nalbert foi uma peça fundamental na conquista do ouro olímpico em Atenas 2004, deixando uma marca indelével na história do vôlei brasileiro.
+	</p>
+
+	<p> 6. Giovane Gávio<br>
+	Títulos: Duas vezes medalhista de ouro olímpico (1992, 2004), campeão mundial e da Liga Mundial.<br>
+	Equipes Notáveis: Cruzeiro, Treviso Volley.<br>
+	Curiosidades: Além de suas conquistas no vôlei de quadra, Giovane também teve sucesso no vôlei de praia, demonstrando sua versatilidade como atleta.
+	</p>
+
+	<p> 7. Ricardinho<br>
+	Títulos: Medalhista de ouro olímpico (2004), campeão mundial e da Liga Mundial.<br>
+	Equipes Notáveis: Sada Cruzeiro, Pallavolo Modena.<br>
+	Curiosidades: Conhecido por sua habilidade como levantador e sua visão de jogo excepcional, Ricardinho foi uma peça-chave na conquista do ouro olímpico em Atenas 2004.
+	</p>
+
+	<p> 8. Murilo Endres<br>
+	Títulos: Medalhista de ouro olímpico (2016), campeão mundial e da Liga Mundial.<br>
+	Equipes Notáveis: SESI-SP, Sada Cruzeiro.<br>
+	Curiosidades: Murilo foi um dos jogadores mais versáteis do vôlei brasileiro, destacando-se tanto na defesa quanto no ataque.
+	</p>
+
+	<p> 9. Maurício Lima<br>
+	Títulos: Medalhista de ouro olímpico (1992), campeão mundial e da Liga Mundial.<br>
+	Equipes Notáveis: Pirelli Volley, Sisley Treviso.<br>
+	Curiosidades: Maurício foi uma das peças-chave da lendária geração de ouro do vôlei brasileiro nos anos 90.
+	</p>
+
+	<p> 10. André Nascimento<br>
+	Títulos: Medalhista de ouro olímpico (2004), campeão mundial e da Liga Mundial.<br>
+	Equipes Notáveis: Sada Cruzeiro, Dinamo Moscow.<br>
+	Curiosidades: André era conhecido por sua potência no ataque e foi fundamental na conquista do ouro olímpico em Atenas 2004.
+	</p>
+	<?php 
+    	include 'footer.php';
+  	?>
 </body>
 </html>
