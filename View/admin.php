294fee67dd3aa3f78c49d585a19e82b0877539a7
JayOta <otavio.cvilela@gmail.com>
2024-08-01 09:15:53 -0300

    09:16 - 01/08

--- View/admin.php
+++ View/admin.php
@@ -2,37 +2,77 @@
 <html>
 
 <head>
- 
-    <link rel="stylesheet" type="text/css" href="css/admin.css">
- 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="css/inicial.css">
+  <link rel="stylesheet" href="css/navbar.css">
+  <link rel="stylesheet" href="css/admin.css">
 </head>
 
 <body>
-  <form>
-    <main>
-            <button id="openSidebarBtn" onclick="openSidebar()" style="display:none;"><img src="./img/menu-line.png"></button>
-            <div id="sidebar" class="sidebar">
+
+  <?php 
+    include 'navbar.php';
+  ?>  
+  <main>  
+    <form>
+        <button id="openSidebarBtn" onclick="openSidebar()" style="display:none;"><img src="./img/menu-line.png"></button>
+        <div id="sidebar" class="sidebar">
                 <button id="closeSidebarBtn" onclick="closeSidebar()"><img src="./img/close-fill.png"></button>
-                <div class="line">
-                    <button class="links"><img class="p-img"  src="./img/dashboard-fill.png" alt="">Painel</button>
-                </div>
-                <div class="line">
-                    <button class="links"><img class="p-img" src="./img/group-line.png">Clientes</button>
-                </div>
-                <div class="line">
-                    <button class="links"><img class="p-img" src="./img/shopping-cart-fill.png" alt="">Produtos</button>
-                </div>
-                <div class="line">
-                    <button class="links"><img class="p-img"  src="./img/bar-chart-fill.png" alt="">Estatísticas</button>
-                </div>
-                <hr>
-                <div class="line">
-                    <button class="links"><img class="p-img"  src="./img/notification-2-fill.png" alt="">Notificações</button>
-                </div>
-                <div class="line">
-                    <button class="links"><img class="p-img"  src="./img/file-3-fill.png" alt="">Documentos</button>
-                </div> 
+            <div class="line">
+                <button class="links"><img class="p-img"  src="./img/dashboard-fill.png" alt="">Painel</button>
             </div>
+            <div class="line">
+                <button class="links"><img class="p-img" src="./img/group-line.png">Clientes</button>
+            </div>
+            <div class="line">
+                <button class="links"><img class="p-img" src="./img/shopping-cart-fill.png" alt="">Produtos</button>
+            </div>
+            <div class="line">
+                <button class="links"><img class="p-img"  src="./img/bar-chart-fill.png" alt="">Estatísticas</button>
+            </div>
+            <hr>
+            <div class="line">
+                <button class="links"><img class="p-img"  src="./img/notification-2-fill.png" alt="">Notificações</button>
+            </div>
+            <div class="line">
+                <button class="links"><img class="p-img"  src="./img/file-3-fill.png" alt="">Documentos</button>
+            </div> 
+        </div>
+        <div class="main-content">
+            <section>
+                <div class="mini-aligns">
+                    <div class="mini-container">Vendas</div>
+                    <div class="mini-container">Receita</div>
+                    <div class="mini-container">Clientes</div>
+                </div>
+                <div class="big-aligns">
+                    <div class="big-container">Relatórios</div>
+                    <div class="big-container">Vendas recentes | Hoje</div>
+                    <div class="big-container">Mais vendidos | Hoje</div>
+                </div> 
+            </section>
+            <aside>
+                <div class="aside-containers">Atividade Recente | Hoje</div>
+                <div class="aside-containers">Notícias e Atualizações | Hoje</div>
+                <div class="aside-containers"></div>
+                <div class="aside-containers"></div>
+            </aside>
+        </div>
+</main>
+    <br><br><br><br><br><br>
+  <?php 
+    include 'footer.php';
+  ?>
+
     <script src="admin.js"></script>
 </body>
 </html>
