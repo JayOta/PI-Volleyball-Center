def8bc91e4ff5be82079320fac24fc929262588c
JayOta <otavio.cvilela@gmail.com>
2024-08-06 09:15:24 -0300

    09:15 - 06/08

--- Controller/login.php
+++ Controller/login.php
@@ -1,9 +1,27 @@
-<?php 
-    require '../Model/login.php';
+<?php
+require '../Model/login.php';
+session_start();
 
-    if($_POST){
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+if ($_POST) {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-        getLogin($email, $senha);
+    if (isset($_POST['login'])) {
+        if (strlen($_POST['email']) == 0 || $_POST['email'] == '') {
+            echo "<script>alert('O seu email não pode ser enviado vazio!');</script>";
+        } else if (strlen($_POST['senha']) == 0 || $_POST['senha'] == '') {
+            echo "<script>alert('A sua senha não pode ser enviado vazio!');</script>";
+        } else {
+            $usuario = getLogin($email, $senha);
+            if (!strcasecmp($usuario['email'], $email) && !strcasecmp($usuario['senha'], $senha)) {
+                $_SESSION['usuario_atual'] = $usuario;
+                $perfil_butao = $_POST['usuario_atual'];
+                header("Location: http://localhost/GitHub/PI-Volleyball-Center/View//inicial.php");
+                
+                
+            } else {
+                echo "<script>alert('Senha ou Email incorretos!');</script>";
+            }
+        }
     }
+}
--- View/navbar.php
+++ View/navbar.php
@@ -5,32 +5,32 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Notícias</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-		crossorigin="anonymous"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="css/navbar.css">
 </head>
 
 <body>
-	<nav class="navbar navbar-expand-lg p-0 m-0" style="background-color: #F9CC02;">
-		<div class="container-fluid" style="background-color: #F9CC02;">
-		<a href="inicial.php">
-        	<img src="img/logo-volei.png" class="logo-img" style="width: 82px; margin-bottom: 2px; margin-top: 2px; padding-bottom: 2px; padding-top: 2px;">
-      	</a>
+	<form method="post">
+		<nav class="navbar navbar-expand-lg p-0 m-0" style="background-color: #F9CC02;">
+			<div class="container-fluid" style="background-color: #F9CC02;">
+				<a href="inicial.php">
+					<img src="img/logo-volei.png" class="logo-img" style="width: 82px; margin-bottom: 2px; margin-top: 2px; padding-bottom: 2px; padding-top: 2px;">
+				</a>
 
-			<header>
-				<nav class="navigation">
-					<a id="inicial" href="inicial.php">Início</a>
-					<a id="link-2" href="fundamentos.php">Fundamentos</a>
-					<a id="link-3" href="noticias.php">Notícias</a>
-					<a id="link-4" href="loja.php">Loja</a>
-					<a id="login" href="index.php" style="bottom: 13px;"><button type="submit" class="btnLogin-popup">Login</button></a>
-				</nav>
-			</header>
-		</div>
-		</div>
-	</nav>
+				<header>
+					<nav class="navigation">
+						<a id="inicial" href="inicial.php">Início</a>
+						<a id="link-2" href="fundamentos.php">Fundamentos</a>
+						<a id="link-3" href="noticias.php">Notícias</a>
+						<a id="link-4" href="loja.php">Loja</a>
+						<a id="login" href="index.php" style="bottom: 13px;"><button type="submit" class="btnLogin-popup" name="usuario_atual">Login</button></a>
+					</nav>
+				</header>
+			</div>
+			</div>
+		</nav>
+	</form>
 </body>
+
 </html>
