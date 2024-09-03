35e079145b5591e43d608e73c241092d2e48874a
JayOta <otavio.cvilela@gmail.com>
2024-09-02 21:41:17 -0300

    21:41 - 02/09

--- Controller/cadastro.php
+++ Controller/cadastro.php
@@ -1,11 +1,11 @@
-<?php 
-    require '../Model/cadastro.php';
+<?php
+require '../Model/cadastro.php';
 
-    if($_POST){
-        $usuario = $_POST['usuario'];
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+if ($_POST) {
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-        getCadastro($usuario, $email, $senha);
-        header("Location: http://localhost/GitHub/PI-Volleyball-Center/View/index.php");
-        }
+    getCadastro($usuario, $email, $senha);
+    // header("Location: http://localhost/GitHub/PI-Volleyball-Center/View/index.php");
+}
