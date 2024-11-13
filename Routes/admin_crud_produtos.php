8063cc5fd71fc95c7c3f1adc17f41445a94e1b0e
JayOta <otavio.cvilela@gmail.com>
2024-11-13 12:02:32 -0300

    Update 'admin.php'

--- Routes/admin_crud_produtos.php
+++ Routes/admin_crud_produtos.php
@@ -8,42 +8,101 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../Routes/css/admin.css">
-    <link rel="stylesheet" href="./css/admin_crud_produtos.css">
+    <link rel="stylesheet" href="./Routes/css/admin-crud-produtos.css">
     <link rel="stylesheet" href="../Routes/css/perfil-on-navbar.css">
     <title>CRUD PRODUTOS</title>
+
+    <style>
+        .crud-opcoes {
+            display: flex;
+            flex-direction: row;
+            gap: ;
+            justify-content: center;
+            align-items: center;
+        }
+
+        button.btn {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 2rem;
+        }
+    </style>
 </head>
 
 <body>
 
     <?php include '../Routes/perfil-on-navbar.php'; ?>
-    <button id="openSidebarBtn" onclick="openSidebar()" style="display:none;"><i class='bx bx-menu'></i></button>
-    <div id="sidebar" class="sidebar">
-        <button id="closeSidebarBtn" onclick="closeSidebar()"><i class='bx bx-x'></i></button>
-        <div class="line">
-            <a href="./admin.php" style="text-decoration: none;"><button class="links"><i class='bx bx-left-arrow'></i>Voltar</button></a>
-        </div>
-        <div class="line">
-            <button class="links"><i class='bx bxs-dashboard'></i>Categorias</button>
-        </div>
-        <div class="line">
-            <button class="links"><i class='bx bx-user'></i>Clientes</button>
-        </div>
-        <div class="line">
-            <button class="links"><i class='bx bx-cart'></i>Produtos</button>
-        </div>
-        <div class="line">
-            <button class="links"><i class='bx bx-bar-chart-alt-2'></i>CRUD Produtos</button>
-        </div>
-        <hr>
-        <div class="line">
-            <button class="links"><i class='bx bx-bell'></i>Notificações</button>
-        </div>
-        <div class="line">
-            <button class="links"><i class='bx bx-file-blank'></i>Páginas</button>
-        </div>
-    </div>
 
+    <div style="display: flex; flex-direction: row; gap: 1rem;">
+        <div class="sidebar-part">
+            <button id="openSidebarBtn" onclick="openSidebar()" style="display:none;"><i class='bx bx-menu'></i></button>
+            <div id="sidebar" class="sidebar">
+                <button id="closeSidebarBtn" onclick="closeSidebar()"><i class='bx bx-x'></i></button>
+                <div class="line">
+                    <a href="./admin.php" style="text-decoration: none;" target="_blank"><button class="links"><i class='bx bx-left-arrow'></i>Voltar</button></a>
+                </div>
+                <div class="line">
+                    <button class="links"><i class='bx bxs-dashboard'></i>Categorias</button>
+                </div>
+                <div class="line">
+                    <button class="links"><i class='bx bx-user'></i>Clientes</button>
+                </div>
+                <div class="line">
+                    <button class="links"><i class='bx bx-cart'></i>Produtos</button>
+                </div>
+                <div class="line">
+                    <button class="links"><i class='bx bx-bar-chart-alt-2'></i>CRUD Produtos</button>
+                </div>
+                <hr>
+                <div class="line">
+                    <button class="links"><i class='bx bx-bell'></i>Notificações</button>
+                </div>
+                <div class="line">
+                    <button class="links"><i class='bx bx-file-blank'></i>Páginas</button>
+                </div>
+            </div>
+        </div>
+        <main style="padding: 2rem 0 0 0.6rem;">
+            <div class="inputs" style="display: flex; flex-direction: column;">
+                <div class="crud-opcoes" style="display: flex; flex-direction: row; gap: 1rem;">
+                    <h1 style="margin-bottom: 1rem; font-size: 2.5rem;">Adicionar Produtos</h1>
+                    <div class="botoes" style="display: flex; flex-direction: row; gap: 0.2rem;">
+                        <button class="btn btn-outline-dark active">Adicionar</button>
+                        <button class="btn btn-outline-dark">Editar</button>
+                        <button class="btn btn-outline-dark">Remover</button>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Nome</span>
+                    <input type="text" class="form-control" name="" aria-describedby="basic-addon1" style="width: 30rem;">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Descrição</span>
+                    <input type="text" class="form-control" name="" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Preço</span>
+                    <input type="number" class="form-control" name="" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Imagem</span>
+                    <input type="image" class="form-control" name="" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Categoria</span>
+                    <input type="text" class="form-control" name="" aria-describedby="basic-addon1">
+                    <input class="send-crud btn btn-outline-success" type="submit" value="Aplicar">
+                </div>
+            </div>
+        </main>
+    </div>
+    <div class="footer-admin" style="position: relative; top: 15.2  rem;">
+        <?php include '../Routes/footer.php'; ?>
+    </div>
     <script src="admin.js"></script>
 </body>
 
